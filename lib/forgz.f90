4583218ab3dd517d278f01b2c4f187583a158078
Baudilio Tejerina (@GFDL) <baudilio.tejerina@noaa.gov>
2020-10-26 18:36:21 -0400

    fix dir-local.el to prevent TAB in f90 files

--- lib/forgz.f90
+++ lib/forgz.f90
@@ -18,13 +18,13 @@ MODULE forgz
      INTEGER(c_int) FUNCTION gzread(filename, buffer, length) BIND(c, name="gzread")
        USE, INTRINSIC :: iso_c_binding, ONLY: c_ptr, c_char, c_int
        IMPLICIT NONE
-       TYPE(c_ptr), VALUE :: filename
-       CHARACTER(kind=c_char), DIMENSION(*) :: buffer
-       INTEGER(c_int), VALUE :: length
+       TYPE(c_ptr), VALUE :: filename ! gzFile file
+       CHARACTER(kind=c_char), DIMENSION(*) :: buffer !  voidp buf
+       INTEGER(c_int), VALUE :: length ! unsigned len
      END FUNCTION gzread
   END INTERFACE
 
-    INTERFACE
+  INTERFACE
      INTEGER(c_int) FUNCTION gzgets(filename, buffer, length) BIND(c)
        USE, INTRINSIC :: iso_c_binding, ONLY: c_ptr, c_char, c_int
        IMPLICIT NONE
@@ -56,24 +56,32 @@ CONTAINS
     INTEGER, INTENT(out) :: status
 
     ! -- Local Variables
-    TYPE(c_ptr) :: inp
-    INTEGER(c_int) :: ierr
-
+    TYPE(c_ptr) :: gzFile
+    INTEGER(c_int) :: nbytes ! number of uncompresses bytes actually read.
+    INTEGER(Kind=c_int) :: len
+    INTEGER(Kind=c_int) :: ierr
 
     ! ---
 
     status = 1
 
-	! - open file
-    INP = gzopen(TRIM(filename)//c_null_char, "rb")
-    IF (.not. c_ASSOCIATED(inp)) RETURN ! BTA: sanity check??
+    ! - open file
+    gzFile = gzopen(TRIM(filename)//c_null_char, "rb")
+    IF (.NOT. c_ASSOCIATED(inp)) RETURN ! BTA: sanity check??
 
-	! read data
-     ierr = gzread(INP, buffer, SIZE(buffer))
-    ! IF (ierr /= 0) RETURN ! TODO: differenciate and handle EOF
+    ! - read data
+    len = SIZE(buffer, KIND=c_int)
+    nbytes = gzread(gzFile, buffer, len)
+    !IF (ierr /= 0) RETURN ! TODO: differenciate and handle EOF
+    !IF (nbytes < len) THEN
+    !   BTA: END of DATA reached!
+    !   BTA: this isnt an error.
+    !   BTA: but I have to take action, e.g., notify (by returning the event somehow) and CLOSE the file.
+    !   BTA: otherwise gzread() will CONTINUE reading IF called again.
+    !END IF
 
 	! close
-    ierr = gzclose(INP)
+    ierr = gzclose(gzFile)
     IF (ierr /= 0) RETURN
 
     ! No errors
